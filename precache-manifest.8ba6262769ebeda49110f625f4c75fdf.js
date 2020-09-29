4845fe74b85cede11365152275c04217317a87c1
Frank Camara <frankcamara@Franks-MacBook-Pro.local>
2020-09-29 22:39:46 +0200

    Updates

---
+++ precache-manifest.8ba6262769ebeda49110f625f4c75fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e614955c1a7cbc06042a9e4098c162f",
+    "url": "/hubbie/index.html"
+  },
+  {
+    "revision": "b20a778881e26766a26e",
+    "url": "/hubbie/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f752780509f292b9b4c",
+    "url": "/hubbie/static/js/2.4cdc8102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hubbie/static/js/2.4cdc8102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20a778881e26766a26e",
+    "url": "/hubbie/static/js/main.a04643b4.chunk.js"
+  },
+  {
+    "revision": "5f52198316bc816ddc6c",
+    "url": "/hubbie/static/js/runtime-main.b93ad5ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hubbie/static/media/logo.5d5d9eef.svg"
+  }
+]);
